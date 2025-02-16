083a47de0fc5f1bf73ec8b6a6dad7d4a8e897d61
Ahmad30Jalal <115393713+Ahmad30Jalal@users.noreply.github.com>
2025-02-16 14:37:18 +0500

    Update 2.cpp

--- Lab 3/2.cpp
+++ Lab 3/2.cpp
@@ -2,70 +2,70 @@
 #include <string>
 using namespace std;
 
-class WeekDays{
+class WeekDays {
 
-    private:
-        string days[7];
-        int current_day;
+private:
+    string days[7];
+    int current_day;
 
-    public:
-
-    WeekDays (){    
-days[0]="Monday";        
-        current_day = 1;
+public:
+    WeekDays() {
+        days[0] = "Monday";
+        days[1] = "Tuesday";
+        days[2] = "Wednesday";
+        days[3] = "Thursday";
+        days[4] = "Friday";
+        days[5] = "Saturday";
+        days[6] = "Sunday";
+        current_day = 0; 
     }
 
+    
     WeekDays(int current_day) {
-        days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday" ,"Saturday", "Sunday"};
+        days[0] = "Monday";
+        days[1] = "Tuesday";
+        days[2] = "Wednesday";
+        days[3] = "Thursday";
+        days[4] = "Friday";
+        days[5] = "Saturday";
+        days[6] = "Sunday";
 
         if (current_day > 7) {
             current_day = current_day % 7;
         }
+        this->current_day = current_day - 1; 
     }
 
-    void get_current_day(int current_day) {
+    void get_current_day() {
         cout << "Current day: " << days[current_day] << endl;
     }
 
-    void get_next_day(int current_day) {
-        cout << "Next day: " << days[current_day + 1] << endl;
+    void get_next_day() {
+        int next_day = (current_day + 1) % 7;
+        cout << "Next day: " << days[next_day] << endl;
     }
 
-    void get_previous_day(int current_day) {
-        cout << "Previous day: " << days[current_day - 1] << endl;
+    void get_previous_day() {
+        int previous_day = (current_day - 1 + 7) % 7;
+        cout << "Previous day: " << days[previous_day] << endl;
     }
 
-    void get_nday(int current_day) {
+    void get_nday() {
         int n;
-        cout << "Current day: " << current_day << endl;
+        cout << "Current day: " << days[current_day] << endl;
         cout << "Enter value of n: ";
         cin >> n;
-        int new_day = n + current_day;
-        if ((new_day) <= 7) {
-            cout << "Day after N days: " << days[new_day] << endl;
-        }
-        else {
-            do {
-                new_day -= 7;
-            } while(new_day <= 7);
-        }
+        int new_day = (current_day + n) % 7;
         cout << "Day after N days: " << days[new_day] << endl;
-        }
-
-
-    
-
-    
-
+    }
 };
 
 int main() {
-
-    WeekDays w1(5);
-    w1.get_current_day(5);
-    w1.get_next_day(5);
-    w1.get_previous_day(5);
-    w1.get_nday(3);
+    WeekDays w1(5); 
+    w1.get_current_day();
+    w1.get_next_day();
+    w1.get_previous_day();
+    w1.get_nday();
 
     return 0;
 }
